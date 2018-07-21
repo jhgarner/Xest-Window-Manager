c6552d5e19141608b41205a8fc5c1847066cc7a9
jkrmnj <jkrmnj465@gmail.com>
2018-07-20 20:48:05 -0600

    Add real horizontal tiling and some comments
    The WM will now store a list of all visible windows and will tile them
    horizontally when one is created or destroyed.

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,6 +11,7 @@ import Graphics.X11.Xlib.Window
 import Graphics.X11.Xlib.Types
 import Graphics.X11.Xlib.Event
 import Graphics.X11.Xlib.Extras
+import Graphics.X11.Xlib.Screen
 import Graphics.X11.Types
 import Data.Bits
 import Data.List
@@ -18,61 +19,96 @@ import Data.Text
 import Data.Text.IO as T
 import Control.Monad.IO.Class
 import Control.Monad.Reader
+import Control.Monad.State.Class
+import Control.Monad.Trans.State.Strict
+import Foreign.C.Types
 
+
+-- Elements of the global state modifiable by an event
+data EventState = ES { wins :: [Window]
+                     }
+
+-- Immutable state for one iteration of the main loop. Mutates between iterations
 data IterationState = IS {display :: Display
-                         , numWindows :: Int
+                         , eventState :: EventState
                          , event :: Event
+                         , dimensions :: (Dimension, Dimension)
                          }
 
-newtype Xest a = Xest {runXest :: ReaderT IterationState IO a}
+-- An MTL thing because why not? (Not rhetorical)
+newtype Xest a = Xest {runXest :: ReaderT  IterationState IO a}
   deriving (Functor, Applicative, Monad, MonadIO, MonadReader IterationState)
 
+-- TODO don't use someFunc
 someFunc :: IO ()
 someFunc = do
   display <- openDisplay ":99"
   let rootWindow = defaultRootWindow display
   selectInput display rootWindow (substructureNotifyMask .|. substructureRedirectMask)
-  mainLoop display 0
+  mainLoop display $ ES []
 
-mapResponse :: Xest Int
+-- Called on window creation
+mapResponse :: Xest EventState
 mapResponse = do
-  IS {..} <- ask
+  (IS display ES{..} event resolution) <- ask
   liftIO $ do
     (mapWindow display (ev_window event))
-    moveWindow display (ev_window event) (fromIntegral numWindows * 100) 0
-    print "Mapping"
-    return $ numWindows + 1
+    let remainingList = (ev_window event) : wins
+    let numWins = Data.List.length remainingList
+    Data.List.foldl (\acc (i, w) -> acc >> tileHorizontally display w resolution i numWins)
+      (print "mapping") (Data.List.zip [0..] remainingList)
+    return $ ES remainingList
 
-configureResponse :: Xest Int
+-- Essentially just an identity function
+configureResponse :: Xest EventState
 configureResponse = do
   IS {..} <- ask
   let wc = WindowChanges (ev_x event) (ev_y event) (ev_width event) (ev_height event) (ev_border_width event) (ev_above event) (ev_detail event)
   liftIO $ do
     configureWindow display (ev_window event) (ev_value_mask event) wc
-    print numWindows
-    return numWindows
+    print "Configuring"
+    return eventState
 
-destroyAction :: Xest Int
-destroyAction = doNothing "Destroying" >>= return . (-) 1
+-- Called on window destruction
+unmapAction :: Xest EventState
+unmapAction = do
+  IS display ES{..} event resolution <- ask
+  liftIO $ do
+    let remainingList = delete (ev_window event) wins
+    let numWins = Data.List.length remainingList
+    Data.List.foldl (\acc (i, w) -> acc >> tileHorizontally display w resolution i numWins)
+      (print "unmapping") (Data.List.zip [0..] remainingList)
+    return $ ES remainingList
 
-mainLoop :: Display -> Int -> IO ()
-mainLoop d numW = (allocaXEvent $ \xPtr -> do
+-- The recursive main loop of the function
+mainLoop :: Display -> EventState -> IO ()
+mainLoop d es = (allocaXEvent $ \xPtr -> do
   nextEvent d xPtr
   e <- getEvent xPtr
   eType <- get_EventType xPtr
+  let screen = defaultScreenOfDisplay d
   case Data.List.find ((==) eType . fst) handlers of
-    Nothing -> print "Unknown" >> return numW
-    Just (_, f) -> runReaderT (runXest f) (IS d numW e)
+    Nothing -> print "Unknown" >> return es
+    Just (_, f) -> runReaderT (runXest f) (IS d es e (widthOfScreen screen, heightOfScreen screen))
   ) >>= mainLoop d
 
-handlers :: [(EventType, Xest Int)]
-handlers = [(createNotify, doNothing "Destroying")
-           , (destroyNotify, destroyAction)
+-- A list of event handlers
+handlers :: [(EventType, Xest EventState)]
+handlers = [(createNotify, doNothing "Creating")
+           , (destroyNotify, doNothing "Destroying")
            , (configureRequest, configureResponse)
+           , (unmapNotify, unmapAction)
            , (mapRequest, mapResponse)]
 
-doNothing :: Text -> Xest Int
+-- A simple handler for printing and returning
+doNothing :: Text -> Xest EventState
 doNothing t = do
   IS {..} <- ask
   liftIO $ T.putStrLn t
-  return numWindows
+  return eventState
+
+-- TODO Make this signature less terrible
+tileHorizontally :: Display -> Window -> (Dimension, Dimension) -> Position -> Int -> IO ()
+tileHorizontally display w (width, height) i numWins = do
+  moveWindow display w (fromIntegral width `div` fromIntegral numWins * i) 0
+  resizeWindow display w (fromIntegral width `div` fromIntegral numWins) $ fromIntegral height
