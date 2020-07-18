87f57d16b73a81a8c560a9293ea30c99cf12307c
Jack Garner <jgarner@mines.edu>
2020-07-18 14:06:56 -0600

    Working arch package

--- config/startup.sh
+++ config/startup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
-xrdb -merge /home/jack/.Xresources
-xrandr --output eDP-1 --primary --mode 2560x1440
-setxkbmap -layout 3l &
-compton &
-dunst &
-/home/jack/.fehbg &
-(sleep 1 && polybar example &> /home/jack/polybar.txt) &
+
+# The commented out lines give some examples of things that belong here.
+
+# xrdb -merge ~/.Xresources
+# xrandr --output eDP-1 --primary --mode 2560x1440
+# setxkbmap -layout 3l &
+# compton &
+# dunst &
+# ~/.fehbg &
+# (sleep 1 && polybar example &> /dev/null) &
